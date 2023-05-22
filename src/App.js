102a2b9bff4efd2a2f975c4bcadde20456a3b593
HariJW <harshwaychal936@gmail.com>
2023-05-22 11:40:17 +0530

    Tast Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Home from "./components/Home/Home";
+import ViewRecord from "./components/ViewRecord/ViewRecord";
+import AddRecord from "./components/AddRecord/AddRecord";
+import EditRecord from "./components/EditRecord/EditRecord";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/viewRecord" element={<ViewRecord />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/addRecord" element={<AddRecord />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="editRecord/:id" element={<EditRecord />}></Route>
+      </Routes>
     </div>
   );
 }
